37bec55feaf669794609a091a00aa3165aeedd4a
tonidhaliwal <tonidhaliwal@googlemail.com>
2025-02-24 14:34:57 +0100

    added more style

--- frontend/src/lib/Footer.svelte
+++ frontend/src/lib/Footer.svelte
@@ -1,5 +1,10 @@
+<script>
+
+    
+</script>
+
 <div class="mx-26 py-5  items-center justify-center flex">
-<a class="mx-4" href="/">Impressum</a>
+<a class="mx-4" href="/impressum">Impressum</a>
 <a class="mx-4" href="/">Kontakt</a>
 
 </div>
---
+++ frontend/src/routes/impressum/+page.svelte
@@ -0,0 +1,6 @@
+<div class="py-20 px-40">
+<p class="underline">Kontakt</p>
+<p>Tajinder Singh Dhaliwal</p>
+<p>Vogelsangstr.23, Hanau 63457</p>
+<p>tonidhaliwal@googlemail.com</p>
+</div>
